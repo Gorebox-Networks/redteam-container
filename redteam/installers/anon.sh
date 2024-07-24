2a8b43a6e5904bda87c2033b0af98339a653effd
davidfortytwo <85337431+davidfortytwo@users.noreply.github.com>
2024-07-24 11:26:50 +0200

    Update anon.sh

--- redteam/installers/anon.sh
+++ redteam/installers/anon.sh
@@ -7,8 +7,14 @@ mkdir $WORKDIR
 
 echo "Installing anonymity tools"
 apt-get update
-apt-get install -y openvpn torsocks proxychains wireguard
+apt-get install -y openvpn proxychains wireguard
+# Disabled: torsocks
+
 cd $WORKDIR
 git clone $GITHUB/SusmithKrishnan/torghost.git && cd torghost && pip3 install -r requirements.txt
+cd $WORKDIR
+mkdir volana && cd volana
+curl -lO -L https://github.com/ariary/volana/releases/latest/download/volana
+chmod +x volana
 cd $WORKDIR
 echo "Done"
