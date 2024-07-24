cebb0bfe792d476b004d91d242820ad246ced0c9
davidfortytwo <85337431+davidfortytwo@users.noreply.github.com>
2024-07-24 12:32:17 +0200

    Update vulnscan.sh

--- redteam/installers/vulnscan.sh
+++ redteam/installers/vulnscan.sh
@@ -1,28 +1,63 @@
 #!/bin/bash
 
+set -e
+
 WORKDIR="/root/RedTeamToolkit/vulnscan"
 GITHUB="https://github.com"
 
-mkdir $WORKDIR
+# Create working directory if it doesn't exist
+mkdir -p "$WORKDIR"
 
 echo "Installing vulnerability scan tools"
+
+# Update and install necessary packages
 apt update && apt install -y libsasl2-dev libldap2-dev golang-go
-cd $WORKDIR
-git clone $GITHUB/SecureAuthCorp/impacket.git && cd impacket && python3 -m pip install . && cd $WORKDIR
-git clone $GITHUB/darkarnium/Log4j-CVE-Detect.git
-git clone $GITHUB/chenjj/espoofer.git && cd espoofer && pip3 install -r requirements.txt && cd $WORKDIR
-git clone $GITHUB/GlobalCyberAlliance/DomainSecurityScanner.git && cd DomainSecurityScanner && make && cd $WORKDIR
-git clone $GITHUB/vavkamil/dkimsc4n.git && cd dkimsc4n.git && pip3 install -r requirements.txt && cd $WORKDIR
-git clone $GITHUB/drwetter/testssl.sh.git
-git clone $GITHUB/https://github.com/e1abrador/SpringCorePoC.sh.git
-#git clone $GITHUB/projectdiscovery/nuclei.git
-#cd $WORKDIR/projectdiscovery && \ 
-#    cd nuclei/v2/cmd/nuclei; \
-#    go build; \
-#    mv nuclei /usr/local/bin/; \
-#    nuclei -version;
-#    nuclei -ut;
+
+# Function to clone a repository if it doesn't exist
+clone_repo() {
+    local repo_url=$1
+    local target_dir=$2
+    if [ ! -d "$target_dir" ]; then
+        git clone "$repo_url" "$target_dir"
+    else
+        echo "Repository $repo_url already cloned in $target_dir"
+    fi
+}
+
+# Change to working directory
+cd "$WORKDIR"
+
+# Clone and setup repositories
+clone_repo "$GITHUB/SecureAuthCorp/impacket.git" "$WORKDIR/impacket"
+if [ -d "$WORKDIR/impacket" ]; then
+    cd "$WORKDIR/impacket"
+    python3 -m pip install .
+fi
+
+clone_repo "$GITHUB/darkarnium/Log4j-CVE-Detect.git" "$WORKDIR/Log4j-CVE-Detect"
+clone_repo "$GITHUB/chenjj/espoofer.git" "$WORKDIR/espoofer"
+if [ -d "$WORKDIR/espoofer" ]; then
+    cd "$WORKDIR/espoofer"
+    pip3 install -r requirements.txt
+fi
+
+clone_repo "$GITHUB/GlobalCyberAlliance/DomainSecurityScanner.git" "$WORKDIR/DomainSecurityScanner"
+if [ -d "$WORKDIR/DomainSecurityScanner" ]; then
+    cd "$WORKDIR/DomainSecurityScanner"
+    make
+fi
+
+clone_repo "$GITHUB/vavkamil/dkimsc4n.git" "$WORKDIR/dkimsc4n"
+if [ -d "$WORKDIR/dkimsc4n" ]; then
+    cd "$WORKDIR/dkimsc4n"
+    pip3 install -r requirements.txt
+fi
+
+clone_repo "$GITHUB/drwetter/testssl.sh.git" "$WORKDIR/testssl.sh"
+clone_repo "$GITHUB/e1abrador/SpringCorePoC.sh.git" "$WORKDIR/SpringCorePoC"
+
+# Install nuclei
 go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
 nuclei -ut
-cd $WORKDIR
+
 echo "Done"
